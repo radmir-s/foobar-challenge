f347e0bdbef93eee7b3d37fc539d68d3ae6dbd8a
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2020-12-28 15:08:24 -0600

    A better and fast solution.

--- Lucky 3.py
+++ Lucky 3.py
@@ -1,15 +1,12 @@
 def solution(l):
-    count = 0
     n = len(l)
-    for i in range(n-2):
-        for j in range(i+1, n-1):
-            if not l[j] % l[i]:
-                for k in range(j+1,n):
-                    if not l[k] % l[j]:
-                        count += 1
-    return count
-
-
-
-print(solution([1]))
-
+    p = [0]
+    q = [0]
+    for k in range(1,n):
+        p.append(0)
+        q.append(0)
+        for j in range(k):
+            if not l[k] % l[j]:
+                p[k] += 1
+                q[k] += p[j]
+    return sum(q)
