53171f86ed7b6a522eb8fa40e2d31d17774b8568
Radmir Sultamuratov <sultamuratov.r@gmail.com>
2021-01-01 22:22:55 -0600

    Added all-pairs shortest paths matrices

--- rescue_bunnies.py
+++ rescue_bunnies.py
@@ -1,11 +1,25 @@
 from itertools import combinations
 
-def loop(times):
-    for k in range(len(times)):
-        for c in combinations(range(len(times)),k):
-            sum = times[c[k-1]][c[0]]
-            for i in range(k-1):
-                sum += times[c[i]][c[i+1]]
-            if sum < 0:
-                return True
-    return False
+
+def solutions(W, time):
+    # checking for negative cycles
+    n = len(W)
+    for k in range(2, n + 1):
+        for c in combinations(range(n), k):
+            cycle_dist = W[c[k - 1]][c[0]]
+            for i in range(k - 1):
+                cycle_dist += W[c[i]][c[i + 1]]
+            if cycle_dist < 0:
+                return list(range(n - 2))
+
+    # Floyd-Warshall
+    D = W  # shortest paths distance matrix
+    P = [[i if i != j else None for j in range(n)] for i in range(n)] # predecessors matrix
+    for k in range(n):
+        for i in range(n):
+            for j in range(n):
+                if D[i][j] > D[i][k] + D[k][j]:
+                    D[i][j] = D[i][k] + D[k][j]
+                    P[i][j] = P[k][j]
+
+    return D, P
